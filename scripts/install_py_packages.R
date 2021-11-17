835e5bf9b0ea03c52cd04ba50abafe8b8ed16e7a
Eva Schreck <eva.schreck@tuteco.de>
2021-11-17 03:12:56 +0100

    feat: added python package awswrangler and psycopg2-binary

--- scripts/install_py_packages.R
+++ scripts/install_py_packages.R
@@ -9,6 +9,8 @@ use_virtualenv(venv_name)
 py_packages <- c("boto3==1.17.106",
     "botocore==1.20.106",
     "s3fs==2021.11.0",
-    "pandas")
+    "pandas",
+    "awswrangler",
+    "psycopg2-binary==2.9.1")
 
 py_install(py_packages, envname = venv_name, method = "virtualenv")
